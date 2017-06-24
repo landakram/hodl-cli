68bbe4660009a6ef97d44b78442a3e6236feafc9
Mark Hudnall <mhudnall@twilio.com>
2017-06-23 22:19:20 -0700

    Update documentation, bump version

--- cli.py
+++ cli.py
@@ -37,7 +37,7 @@ from app import main
 def run(deposit_amount, deposit_interval, min_available_to_trade,
         allocation_percentage):
     """
-    This script automates recurring USD deposits and asset allocation for GDAX. 
+    This script automates recurring USD deposits and asset allocation for GDAX.
 
     When run, it will check whether a deposit needs to be made and, if so, initiate
     the deposit using a linked bank account.
@@ -45,35 +45,36 @@ def run(deposit_amount, deposit_interval, min_available_to_trade,
     Then, if there is enough available USD in the GDAX account, it will buy currencies
     using all of the available USD, given a user-specified asset allocation. Currencies
     are traded at market price at the time the script is run.
-    
+
     This script is meant to be run as a cron. The cron can be run at any interval
-    less than the `deposit-interval` -- I recommend daily. 
-    The script will ensure that deposits are only made every `deposit-interval` 
-    irrespective of how often it is run. Please ensure that you are running 
-    this script from a single thread to avoid duplicating deposits.
-    
+    less than the `deposit-interval` -- I recommend daily.
+    The script will ensure that deposits are only made every `deposit-interval`
+    irrespective of how often it is run. Please make sure that only one
+    instance of gdax_recurring is running at a time to prevent duplicate
+    deposits.
+
     In addition to the CLI options, a few environment variables must be present:
 
     \b
         GDAX_API_KEY
         GDAX_API_SECRET
         GDAX_PASSPHRASE
-    
+
     Installation:
-    
-    \b
-        pip install gdax_recurring
-    
-    Example usage: 
 
     \b
-        export GDAX_API_KEY=<your_api_key> 
-        export GDAX_API_SECRET=<your_api_secret> 
-        export GDAX_PASSPHRASE=<your_passphrase> 
+        pip install gdax_recurring
+
+    Example usage:
+
+    \b
+        export GDAX_API_KEY=<your_api_key>
+        export GDAX_API_SECRET=<your_api_secret>
+        export GDAX_PASSPHRASE=<your_passphrase>
         gdax_recurring -d 100.00 -i 15 -m 50.00 -a LTC 0.5 -a ETH 0.25 -a BTC 0.25
-    
+
     Explanation:
-    
+
         The above invocation will deposit $100.00 every 15 days. In addition, if the GDAX account has at least $50.00 available to trade, all of the available USD will be used to buy other currencies as follows:
 
     \b
